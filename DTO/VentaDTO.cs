beea244f5a59ba65b84e1c63a510676ec4d738bc
TalitaKumi66 <kari.kumi6@gmail.com>
2024-09-23 03:25:37 -0600

    Primer commit

---
+++ DTO/VentaDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class VentaDTO
+    {
+        [Key]
+        public int VENTA_ID { get; set; }
+        public DateTime FECHA { get; set; }
+        public int CLIENTE_ID { get; set; }
+        public string ESTATUS { get; set; }
+        public int PRODUCTO_ID { get; set; }
+        [Required]
+        [Display(Name = "Cantidad")]
+        public int CANTIDAD { get; set; }
+        public decimal DESCUENTO { get; set; }
+        public decimal TOTAL { get; set; }
+    }
+}
